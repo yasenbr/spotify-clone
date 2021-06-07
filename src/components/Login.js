0d5b8e36f65eadcdc1a07e41136755f16d116211
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-06-07 23:53:50 +0300

    start login screen creation

---
+++ src/components/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../css/Login.css";
+
+function Login() {
+  return (
+    <div className="login">
+      <h1>Hello to spotify Clone login</h1>
+      {/*spotify logo*/}
+      {/* login spotify button */}
+    </div>
+  );
+}
+
+export default Login;

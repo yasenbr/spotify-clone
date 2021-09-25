12e358588f549d063e07709a55db20816e8f98c9
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-09-26 00:28:13 +0300

    building sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,21 @@
 import React from "react";
+import SidebardOption from "./SidebardOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SearchIcon from "@mui/icons-material/Search";
+import LibraryMusicIcon from "@mui/icons-material/LibraryMusic";
 import "../css/Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <h1>II am the sidebar </h1>
+      <img
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebardOption Icon={HomeIcon} title="Home" />
+      <SidebardOption Icon={SearchIcon} title="Search" />
+      <SidebardOption Icon={LibraryMusicIcon} title="Your Library" />
     </div>
   );
 }

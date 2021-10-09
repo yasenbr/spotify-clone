66f9944a3d1e433bf7559b2526ff419ea73aa2a7
yasenbr <74763207+yasenbr@users.noreply.github.com>
2021-10-09 23:21:25 +0300

    building footer player

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,53 @@
 import React from "react";
+import PlayCircleOutlineIcon from "@mui/icons-material/PlayCircleOutline";
+import SkipNextIcon from "@mui/icons-material/SkipNext";
+import SkipPreviousIcon from "@mui/icons-material/SkipPrevious";
+import ShuffleIcon from "@mui/icons-material/Shuffle";
+import RepeatIcon from "@mui/icons-material/Repeat";
+import PlaylistPlayIcon from "@mui/icons-material/PlaylistPlay";
+import VolumeDownIcon from "@mui/icons-material/VolumeDown";
+// import VolumeUpIcon from "@mui/icons-material/VolumeUp";
+import { Grid, Slider } from "@mui/material";
+
 import "../css/Footer.css";
 
 function Footer() {
   return (
     <div className="footer">
-      <h1>I am the footer</h1>
+      <div className="footer__left">
+        <img
+          className="footer__albumLogo"
+          src="https://upload.wikimedia.org/wikipedia/en/3/3d/Coldplay_-_A_Head_Full_of_Dreams.png"
+          alt=""
+        />
+        <div className="footer__songInfo">
+          <h4>Yeah</h4>
+          <p>Coldplay</p>
+        </div>
+      </div>
+      <div className="footer__center">
+        <ShuffleIcon className="footer__grey" />
+        <SkipPreviousIcon className="footer__icon footer__grey" />
+        <PlayCircleOutlineIcon
+          fontSize="large"
+          className="footer__icon play__icon"
+        />
+        <SkipNextIcon className="footer__icon footer__grey" />
+        <RepeatIcon className="footer__grey" />
+      </div>
+      <div className="footer__right">
+        <Grid container spacing={2}>
+          <Grid item>
+            <PlaylistPlayIcon />
+          </Grid>
+          <Grid item>
+            <VolumeDownIcon />
+          </Grid>
+          <Grid item xs>
+            <Slider />
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
--- src/context/reducer.js
+++ src/context/reducer.js
@@ -4,8 +4,8 @@ export const initialState = {
   playing: false,
   item: null,
   //remove after dev fnished
-  // token:
-  //   "BQDeYE3mIpaZHZ9t1e4ePDN_REhUP_AtTF83FPjZ19NCuBSxmn175s7Ip1u18iqHTceIgaGTb1rUb_b9h1e8SXd6fbHpCwJOnKoPFgVKbOP-D77LcTkgueYbb58ilBXgtJVHjZXzf5aNLT--oijKBrCTI2q9Qgl1lWpQu-bq-493be5Q",
+  token:
+    "BQDeYE3mIpaZHZ9t1e4ePDN_REhUP_AtTF83FPjZ19NCuBSxmn175s7Ip1u18iqHTceIgaGTb1rUb_b9h1e8SXd6fbHpCwJOnKoPFgVKbOP-D77LcTkgueYbb58ilBXgtJVHjZXzf5aNLT--oijKBrCTI2q9Qgl1lWpQu-bq-493be5Q",
 };
 
 const reducer = (state, action) => {
